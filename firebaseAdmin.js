9ab53f3fffb9b233e2710cee18765a6c85bf6fd2
HimanshuUyt <codewithhimz@gmail.com>
2026-04-21 20:03:34 +0530

    Added Success

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,32 +1,41 @@
-require("dotenv").config();
+// require("dotenv").config();
+// const admin = require("firebase-admin");
+
+// const {
+//   FIREBASE_PROJECT_ID,
+//   FIREBASE_CLIENT_EMAIL,
+//   FIREBASE_PRIVATE_KEY,
+// } = process.env;
+
+// if (!admin.apps.length) {
+//   try {
+//     if (!FIREBASE_PROJECT_ID || !FIREBASE_CLIENT_EMAIL || !FIREBASE_PRIVATE_KEY) {
+//       throw new Error("Firebase ENV missing");
+//     }
+
+//     admin.initializeApp({
+//       credential: admin.credential.cert({
+//         projectId: FIREBASE_PROJECT_ID,
+//         clientEmail: FIREBASE_CLIENT_EMAIL,
+//         privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+//       }),
+//     });
+
+//     console.log("🔥 Firebase Admin Initialized");
+
+//   } catch (error) {
+//     console.error("❌ Firebase Admin Init Error:", error.message);
+//     process.exit(1);
+//   }
+// }
+
+// module.exports = admin;
+
 const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
 
-const {
-  FIREBASE_PROJECT_ID,
-  FIREBASE_CLIENT_EMAIL,
-  FIREBASE_PRIVATE_KEY,
-} = process.env;
-
-if (!admin.apps.length) {
-  try {
-    if (!FIREBASE_PROJECT_ID || !FIREBASE_CLIENT_EMAIL || !FIREBASE_PRIVATE_KEY) {
-      throw new Error("Firebase ENV missing");
-    }
-
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: FIREBASE_PROJECT_ID,
-        clientEmail: FIREBASE_CLIENT_EMAIL,
-        privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-      }),
-    });
-
-    console.log("🔥 Firebase Admin Initialized");
-
-  } catch (error) {
-    console.error("❌ Firebase Admin Init Error:", error.message);
-    process.exit(1);
-  }
-}
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 module.exports = admin;
