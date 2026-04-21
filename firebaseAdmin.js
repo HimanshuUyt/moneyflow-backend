9aa52e60c374e9cf3691ede356f69d4c75d9a114
HimanshuUyt <codewithhimz@gmail.com>
2026-04-21 20:06:56 +0530

    Added Sucess

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,41 +1,12 @@
-// require("dotenv").config();
-// const admin = require("firebase-admin");
-
-// const {
-//   FIREBASE_PROJECT_ID,
-//   FIREBASE_CLIENT_EMAIL,
-//   FIREBASE_PRIVATE_KEY,
-// } = process.env;
-
-// if (!admin.apps.length) {
-//   try {
-//     if (!FIREBASE_PROJECT_ID || !FIREBASE_CLIENT_EMAIL || !FIREBASE_PRIVATE_KEY) {
-//       throw new Error("Firebase ENV missing");
-//     }
-
-//     admin.initializeApp({
-//       credential: admin.credential.cert({
-//         projectId: FIREBASE_PROJECT_ID,
-//         clientEmail: FIREBASE_CLIENT_EMAIL,
-//         privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-//       }),
-//     });
-
-//     console.log("🔥 Firebase Admin Initialized");
-
-//   } catch (error) {
-//     console.error("❌ Firebase Admin Init Error:", error.message);
-//     process.exit(1);
-//   }
-// }
-
-// module.exports = admin;
-
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+
+  console.log("🔥 Firebase Admin Initialized");
+}
 
 module.exports = admin;
